ecc358620266617398a327dfaa4f64ab401acc8b
bjgebbie <Bjgebbie@gmail.com>
2025-03-30 19:25:25 -0500

    color overhaul

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -1,9 +1,17 @@
+-- Tabs
 vim.cmd("set expandtab")
 vim.cmd("set tabstop=4")
 vim.cmd("set softtabstop=4")
 vim.cmd("set shiftwidth=4")
 
+-- Margin line numbers
 vim.cmd("set number relativenumber")
 
+-- Horizontal overflow behavior
 vim.cmd("set nowrap")
 
+-- Telescope colors
+vim.api.nvim_set_hl(0, "TelescopeBorder", { fg = "#E68183" })
+vim.api.nvim_set_hl(0, "TelescopeSelection", { bg = "#27231c" })
+vim.api.nvim_set_hl(0, "TelescopeSelectionCaret", { fg = "#E68183" })
+vim.api.nvim_set_hl(0, "TelescopePromptPrefix", { fg = "#E68183" })
--- lua/plugins/catppuccin.lua
+++ lua/plugins/catppuccin.lua
@@ -3,6 +3,15 @@ return {
 	name = "catppuccin",
 	priority = 1000,
 	config = function()
+		require("catppuccin").setup({
+			color_overrides = {
+				all = {
+					base = "#32352A",
+					mantle = "#27231c",
+					crust = "#27231c",
+				},
+			},
+		})
 		vim.cmd.colorscheme("catppuccin")
 	end,
 }
