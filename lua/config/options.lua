370e4a0d1afedfb6e20f730f2a56d2a6993cee6a
bjgebbie <bjgebbie@gmail.com>
2025-04-03 20:39:20 -0500

    added term

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -46,3 +46,4 @@ end
 
 vim.api.nvim_set_keymap("n", "<C-t>", ":lua toggle_terminal()<CR>i", { noremap = true, silent = true })
 vim.api.nvim_set_keymap("t", "<C-t>", "<C-\\><C-n>:lua toggle_terminal()<CR>", { noremap = true, silent = true })
+vim.api.nvim_set_keymap("t", "<C-w>", "<C-\\><C-n>:winc k<CR>", { noremap = true, silent = true })
